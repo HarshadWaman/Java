bbefe820cdf33111dbed00b63ae10f052fde8b27
HarshadWaman <156565915+HarshadWaman@users.noreply.github.com>
2024-07-16 19:40:21 +0530

    Create  arraySumAverage.java
    This java program give array of sum and average 
    in this program use arrayutility to input array.

---
+++ arraySumAverage.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArraySumAverage{
+    public static void main(String []args) {
+       System.out.println("Welcome to Array sum and Average");
+       int[] numArray = Arrayutility.inputArray();
+        long sum = sum(numArray);
+        int avg = average(numArray);
+        System.out.println("Sum of the number is: "+sum);
+        System.out.println("Average of the numbers is: "+avg);
+   
+   
+    }
+
+    public static long sum(int[] numArray){
+        long sum = 0;
+        int i = 0;
+        while(i < numArray.length){
+            sum += numArray[i];
+            i++;
+        }
+        return sum;
+
+    }
+
+    public static int average(int[] numArray){
+        long sum = sum(numArray);
+        return (int) (sum /numArray.length);
+    }
+}
